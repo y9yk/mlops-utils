0840888a2a7100d07518aa6e03b2d024e050b0e9
y9yk <andrew.yk82@gmail.com>
2023-03-28 09:50:00 +0900

    add constants

---
+++ modules/constants/strings.py
@@ -0,0 +1,22 @@
+import os
+
+# jwt
+JWT_SECRET = os.environ.get("JWT_SECRET")
+JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM", "HS256")
+
+
+# model
+MODEL_NAME = os.environ.get("MLOPS_MODEL_NAME")
+MODEL_STAGE = os.environ.get("MLOPS_MODEL_STAGE")
+
+# exception messages
+INVALID_TOKEN_OR_EXPIRED_TOKEN = "invalid token or expired"
+INVALID_AUTHENTICATION_SCHEME = "invalid authentication scheme"
+INVALID_AUTHORIZATION_CODE = "invalid authorization code"
+AUTHENTICATION_REQUIRED = "authentication required"
+
+# auth
+BEARER = "Bearer"
+
+# http
+# ALLOWED_HOSTS = ["*"]
