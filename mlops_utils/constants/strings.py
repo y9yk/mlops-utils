a34b28e86ea025cebab60c7d6b625399660e9c89
y9yk <andrew.yk82@gmail.com>
2023-03-28 11:59:31 +0900

    remove useless var.

--- mlops_utils/constants/strings.py
+++ mlops_utils/constants/strings.py
@@ -4,10 +4,6 @@ import os
 JWT_SECRET = os.environ.get("JWT_SECRET")
 JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM", "HS256")
 
-# model
-MODEL_NAME = os.environ.get("MLOPS_MODEL_NAME")
-MODEL_STAGE = os.environ.get("MLOPS_MODEL_STAGE", "latest")
-
 # exception messages
 INVALID_TOKEN_OR_EXPIRED_TOKEN = "invalid token or expired"
 INVALID_AUTHENTICATION_SCHEME = "invalid authentication scheme"
@@ -16,6 +12,3 @@ AUTHENTICATION_REQUIRED = "authentication required"
 
 # auth
 BEARER = "Bearer"
-
-# http
-# ALLOWED_HOSTS = ["*"]
